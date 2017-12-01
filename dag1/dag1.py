c2604bbf7a614fccb205370bed07f1bdc1af3f89
Rickard Bark <bark.rickard@gmail.com>
2017-12-01 21:06:36 +0100

    part2 solved

--- dag1/dag1.py
+++ dag1/dag1.py
@@ -4,11 +4,13 @@ with open('input.txt') as file:
     puzzle = file.read()
 
 
-print(puzzle)
 hash= str(puzzle)
 puzzle_length = len(str(hash))
 
-result = sum(int(hash[i]) for i in range(puzzle_length) if hash[i] == hash[(i + 1) % puzzle_length])
+# del 1
+# result = sum(int(hash[i]) for i in range(puzzle_length) if hash[i] == hash[(i + 1) % puzzle_length])
+
+result = sum(int(hash[i]) for i in range(puzzle_length) if hash[i] == hash[(i + puzzle_length//2)  % puzzle_length])
 
 
 print(result)
