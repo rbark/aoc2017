a2c0972ba087dc86d5f81d3b59bf7d9700dbd0fa
Rickard Bark <bark.rickard@gmail.com>
2017-12-01 21:01:53 +0100

    day 1 solved

--- dag1/dag1.py
+++ dag1/dag1.py
@@ -1,11 +1,14 @@
 
 
 with open('input.txt') as file:
-    puzzle = file.readlines()
+    puzzle = file.read()
 
+
+print(puzzle)
 hash= str(puzzle)
 puzzle_length = len(str(hash))
 
 result = sum(int(hash[i]) for i in range(puzzle_length) if hash[i] == hash[(i + 1) % puzzle_length])
+
 
 print(result)
