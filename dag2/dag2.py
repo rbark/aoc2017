a9e8cc21469a9bba19137d8fe176ddfbda9afefb
Rickard Bark <bark.rickard@gmail.com>
2017-12-02 16:26:39 +0100

    dag2 del2

--- dag2/dag2.py
+++ dag2/dag2.py
@@ -2,10 +2,28 @@ with open('input.txt') as file:
     puzzle = file.readlines()
 
 
-checksum = 0
 
-for line in puzzle:
-    numberList = list(map(int, (line.split())))
-    checksum += int(max(numberList)) - int(min(numberList))
+def part1():
 
-print(checksum)
+    checksum = 0
+    for line in puzzle:
+        numberList = list(map(int, (line.split())))
+        checksum += int(max(numberList)) - int(min(numberList))
+
+    print(checksum)
+
+
+def part2():
+    checksum = 0
+    for line in puzzle:
+        numberList = list(map(int, (line.split())))
+        numberList.sort(reverse=True)
+        for i in range(len(numberList)):
+            for j in range(i+1, len(numberList)):
+                if numberList[i] % numberList[j] == 0:
+                    checksum += numberList[i] / numberList[j]
+                    break
+    print(checksum)
+
+
+part2()
