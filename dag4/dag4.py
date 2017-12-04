69d89c779e9ba6ab86cb40cf57af567232555b2b
Rickard Bark <bark.rickard@gmail.com>
2017-12-04 21:36:32 +0100

    dag4 del 1 och 2

---
+++ dag4/dag4.py
@@ -0,0 +1,24 @@
+words = []
+
+valid_lines = 0
+
+# with open("input.txt") as f:
+#     for line in f.readlines():
+#         wordlist = line.strip().split(' ')
+#         wordset = set(wordlist)
+#         if len(wordset) == len(wordlist):
+#             valid_lines += 1
+#
+valid_passphrase = 0
+
+with open("input.txt") as f:
+    for line in f.readlines():
+        wordlist = line.strip().split(' ')
+        sortedChars = [''.join(sorted(w)) for w in wordlist]
+        wordset = set(sortedChars)
+        if len(wordset) == len(wordlist):
+            valid_passphrase += 1
+
+
+# print(valid_lines)
+print(valid_passphrase)
