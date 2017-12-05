17f79760f010b5aabed71fb1062b93f037b6c8e6
Rickard Bark <bark.rickard@gmail.com>
2017-12-05 18:24:10 +0100

    dag 5 del 2

--- dag5/dag5.py
+++ dag5/dag5.py
@@ -6,11 +6,35 @@ with open('input.txt') as file:
 
 current_pos = 0
 
-try:
-    while True:
-        tmp = instructions[current_pos]
-        instructions[current_pos] = tmp+1
-        current_pos += tmp
-        jumps+=1
-except:
-    print(jumps)
+
+def part1():
+    global instructions, current_pos, jumps
+    try:
+        while True:
+            tmp = instructions[current_pos]
+            instructions[current_pos] = tmp + 1
+            current_pos += tmp
+            jumps += 1
+    except:
+        print(jumps)
+
+
+def part2():
+    global instructions, current_pos, jumps
+    jumps = 0
+    try:
+        while True:
+            tmp = instructions[current_pos]
+            if tmp > 2:
+                replace = tmp -1
+            else:
+                replace = tmp + 1
+            instructions[current_pos] = replace
+            current_pos += tmp
+            jumps += 1
+    except:
+        print(jumps)
+
+
+#part1()
+part2()
